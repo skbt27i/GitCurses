a18aaaefd545c6686538bd543c005cbf3530fa95
Santosh <stirumala@ufl.edu>
2021-06-26 16:08:09 -0700

    Add input and title

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 extern crate pancurses;
 
-use pancurses::{initscr, endwin};
-
+use pancurses::{initscr, endwin, Input};
+use std::io::{self, Write};
 fn main() {
   let window = initscr();
-  window.printw("Hello Rust");
+
+  window.draw_box('*', '*');
   window.refresh();
-  window.getch();
+  window.mvaddstr(0, 0, "Welcome to Git Curses, a central place for info about Git repositories!");
+  window.refresh();
+
+  let y =  window.derwin( 3, 50, 10,10 ).unwrap();
+    
+  
+
+  y.draw_box('*', '*');
+  y.addstr("Enter your git repository link here");
+  y.mv(1, 1);
+
+  y.refresh();
+  let mut hello: String = String::new();
+
+  while(true) {
+    match y.getch() {
+      Some(Input::Character(c)) => { hello.push(c); },
+      Some(Input::KeyDC) => break,
+      Some(input) => { window.addstr(&format!("{:?}", input)); },
+      None => ()
+  }
+   y.refresh();
+   window.refresh();
+
+  }
+
   endwin();
 }
