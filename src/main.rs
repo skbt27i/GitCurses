5836aaa9b7bb8d41a3e0edd681406cf10cc021ac
Santosh <stirumala@ufl.edu>
2023-01-14 23:54:27 -0800

    put terminal into infinite loop to allow multiple strings to be searched. still need to enable linux, allow multiple repos to be accessed in one session, print output into terminal, improve user experience

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ extern crate pancurses;
 use std::process::Command;
 
 use git2::Repository;
-use pancurses::{initscr, endwin, Input, noecho};
-use std::io::{self, Write};
+use pancurses::{initscr, endwin, Input};
 
 fn main() {
   let window = initscr();
@@ -19,63 +18,62 @@ fn main() {
   y.draw_box('*', '*');
   y.keypad(true);
 
-  y.addstr("Enter git link, press delete key to submit");
+
+  y.addstr("Enter git link, press delete key to submit.");
   y.mv(1, 1);
 
   y.refresh();
-  let mut hello: String = String::new();
-  let mut t = true;
-  while(t){
+  let mut url: String = String::new();
+  while true {
     match y.getch() {
       Some(Input::KeyDC) => {  break; },
-      Some(Input::Character(c)) => { hello.push(c); },
+      Some(Input::Character(c)) => { url.push(c); },
       Some(input) => { window.addstr(&format!("{:?}", input)); },
       None => ()
+   }
+
+    y.refresh();
+    window.refresh();
   }
-
-  y.refresh();
-  window.refresh();
-}
-
-y.erase();
-y.refresh();
-window.refresh();
-
-  
-
-  let mut search_string: String = String::new();
-
-  let x =  window.derwin( 3, 50, 10,10 ).unwrap();
-
-  x.draw_box('*', '*');
-  x.addstr("Enter search string, press delete key to submit");
-  x.mv(1, 1);
-  while(true) {
-    match x.getch() {
-      Some(Input::Character(c)) => { search_string.push(c); },
-      Some(Input::KeyDC) => break,
-      Some(input) => { window.addstr(&format!("{:?}", input)); },
-      None => ()
-  }
-   x.refresh();
-   window.refresh();
-
-  }
-
-  let url = hello;
-  let mut owned_string: String = "findstr /s /i /n ".to_owned();
-  owned_string.push_str(&search_string);
-  owned_string.push_str(" *.*");
-  let repo = match Repository::clone(&url, "newRepo") {
-      Ok(repo) => repo,
-      Err(e) => panic!("failed to clone: {}", e),
+  let _repo = match Repository::clone(&url, "newRepo") {
+    Ok(_repo) => _repo,
+    Err(e) => panic!("failed to clone: {}", e),
   };
 
-  let output = Command::new("cmd")
-  .args(&["/C", &owned_string])
-  .spawn()
-  .expect("Failed to execute");
+  while true  {
+    y.erase();
+    y.refresh();
+    window.refresh();
 
+    let mut search_string: String = String::new();
+
+    let x =  window.derwin( 3, 50, 10,10 ).unwrap();
+
+    x.draw_box('*', '*');
+    x.addstr("Enter search string, press delete key to submit.");
+    x.mv(1, 1);
+    while(true) {
+        match x.getch() {
+          Some(Input::Character(c)) => { search_string.push(c); },
+          Some(Input::KeyDC) => break,
+          Some(input) => { window.addstr(&format!("{:?}", input)); },
+          None => ()
+      }
+      x.refresh();
+      window.refresh();
+
+      }
+
+      let mut owned_string: String = "findstr /s /i /n ".to_owned();
+      owned_string.push_str(&search_string);
+      owned_string.push_str(" *.*");
+
+
+      let _output = Command::new("cmd")
+      .args(&["/C", &owned_string])
+      .spawn()
+      .expect("Failed to execute");
+  }
   
 
   endwin();
